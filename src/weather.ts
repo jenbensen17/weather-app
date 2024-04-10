778e5341cce191ad342422f346e1b12c5064bb35
Benjamin Jensen <149121554+jenbensen17@users.noreply.github.com>
2024-04-09 19:20:32 -0600

    Set up HTML/CSS, connect to API

---
+++ src/weather.ts
@@ -0,0 +1,13 @@
+// https://api.open-meteo.com/v1/forecast?current=temperature_2m,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone=America%2FDenver
+
+import axios from "axios"
+
+export function getWeather(lat: number, lon: number, timezone: string) {
+    return axios.get("https://api.open-meteo.com/v1/forecast?current=temperature_2m,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timeformat=unixtime" 
+    , { params: {
+        latitude: lat,
+        longitude: lon,
+        timezone,
+    }})
+
+}
